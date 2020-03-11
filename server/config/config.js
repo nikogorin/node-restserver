0a545e8f3a6e9cc4af51c7dcf2d1e220276ae990
Nicolas Piccolini <nicolas.piccolini@engee.com.ar>
2020-03-10 23:46:13 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ process.env.PORT = process.env.PORT || 3000;
 // Entorno
 // ==============================
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // ==============================
 // Base de datos
@@ -19,9 +19,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
-    urlDB = 'mongodb://localhost:27017/cafe'
+    urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafe-user:9p4YGGBShRZ3ycDs@cluster0-ftdjm.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
